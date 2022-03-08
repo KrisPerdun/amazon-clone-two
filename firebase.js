bd913fb4e6d2adb1e1be176d22a104457d37e618
KrisPerdu <krisperdun@gmail.com>
2022-03-07 17:14:52 -0800

    Added-Firebase

---
+++ firebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCYP-3M551nRPnFS0QlX8erwt1gJUu12mQ",
+    authDomain: "clone-two-3857f.firebaseapp.com",
+    projectId: "clone-two-3857f",
+    storageBucket: "clone-two-3857f.appspot.com",
+    messagingSenderId: "569318814328",
+    appId: "1:569318814328:web:972ab660953628746b738d",
+    measurementId: "G-BVLYLVFHEZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
